38d06eea5047ed88704957012375c0430551fb23
Parsa Ahmadi Nasab Emran <parsatempleuniversity@outlook.com>
2025-08-11 18:58:27 -0400

    chore(CountryDetail): add index.ts barrel export for CountryDetail components

---
+++ src/components/CountryDetail/index.ts
@@ -0,0 +1,4 @@
+// Barrel export for all CountryDetail components
+export { default as CountryDetail } from './CountryDetail';
+export { default as BorderCountries } from './BorderCountries';
+export { default as DetailInfo } from './DetailInfo';
